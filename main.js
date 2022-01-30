f177cb72d3d65c9f7f481092e8b585a1775df910
Samin Israr Ravi <saminisrar1@gmail.com>
2022-01-30 11:08:01 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var myToDoList = [ "১) রাত ৮ টা বাজে মডিউল আনলক করো", "২) ফটাফট ভিডিও দেখে দেখে প্রাকটিস করো", "৩) ভিডিও দেখতে দেখতে নোটস নাও ", "৪) মডিউল শেষ হলে পুরা মডিউল নিজে নিজে প্রাকটিস করো ", "৫) কোন কিছু বুঝতে না পারলে সাপোর্ট সেশনে জয়েন করো "];
+   var i = 0;
+
+// while (i < myToDoList.length) {
+ 
+//     var elementList = myToDoList[i];
+//     console.log(elementList);
+//     i++;
+    
+// }
+
+for (var i = 0; i < myToDoList.length; i++) {
+    var elementList = myToDoList[i];
+    console.log(elementList);
+    
+}
